32f2b5afde09c343c7c46f0c1c1a9c19096aefc7
Johannes Charra <johannes.charra@gmx.de>
2014-11-06 22:44:08 +0100

    render some dummy games with. Implement "yourturn" helper func

--- brainme.js
+++ brainme.js
@@ -1,6 +1,10 @@
 Games = new Mongo.Collection("games");
+Questions = new Mongo.Collection("questions");
+Category = new Mongo.Collection("categories");
 
 if (Meteor.isClient) {
+    Meteor.subscribe("games");
+
     Router.route('/', function () {
         this.render('startpage');
     });
@@ -25,13 +29,82 @@ if (Meteor.isClient) {
         }
     });
 
+    Template.game.helpers({
+        yourturn: function () {
+            if (this.player1 == Meteor.user().username) {
+                return this.answers2.length % 3 == 0 && this.answers1.length - this.answers2.length <= 3;
+            } else {
+                return this.answers1.length % 3 == 0 && this.answers1.length > this.answers2.length;
+            }
+        }
+    });
+
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_ONLY"
     });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-        // code to run on server at startup
+    Games.remove({});
+    Questions.remove({});
+
+    var players = ["Joe", "Pete", "Mary", "Alice", "Mike", "Sue"];
+    var gameID = 0;
+
+    for (var i = 0; i < players.length - 1; i++) {
+        for (var j = i + 1; j < players.length; j++) {
+            Games.insert({
+                player1: players[i],
+                player2: players[j],
+                started: new Date(),
+                questions: [],
+                answersP1: [],
+                answersP2: [],
+                gameNumber: gameID++
+            })
+        }
+    }
+
+    Questions.insert({
+        text: "Was ist die Hauptstadt von Deutschland?",
+        answers: ["Prag", "Berlin", "Bonn", "Denzlingen"],
+        correct: 1,
+        category: "geography"
+    });
+
+    Questions.insert({
+        text: "Was ist die Hauptstadt der Schweiz?",
+        answers: ["Prag", "Bern", "Paris", "Basel"],
+        correct: 1,
+        category: "geography"
+    });
+
+    Questions.insert({
+        text: "Was ist die Hauptstadt von Frankreich?",
+        answers: ["Paris", "Bordeaux", "Cannes", "Marseille"],
+        correct: 0,
+        category: "geography"
+    });
+
+    Questions.insert({
+        text: "Was ist die Hauptstadt von England?",
+        answers: ["Prag", "Berlin", "Bonn", "London"],
+        correct: 3,
+        category: "geography"
+    });
+
+    Questions.insert({
+        text: "Was ist die Hauptstadt von Spanien?",
+        answers: ["Prag", "Madrid", "Bonn", "Denzlingen"],
+        correct: 1,
+        category: "geography"
+    });
+
+    Meteor.publish("games", function () {
+        return Games.find({});
+    });
+
+    Meteor.publish("questions", function () {
+        return Questions.find({});
     });
 }
